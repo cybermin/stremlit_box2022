e2ba0110e50ed97f664ca8e5e91d4695d0c19193
cybermin <sillamin@naver.com>
2022-08-10 08:47:07 +0900

    m

--- app.py
+++ app.py
@@ -4,6 +4,38 @@ import numpy as np
 #from datetime import datetime
 import datetime
 
+
+#박스오피스 함수
+def box(targetDt) :
+  apikey = 'f5eef3421c602c6cb7ea224104795888'  
+  targetDt = targetDt.replave('-', '')
+   
+  url = 'http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?'
+  url = url + 'key=' + apikey 
+  url = url + '&targetDt=' + targetDt
+
+  data =  urlopen(url).read().decode()
+  data = json.loads(data) 
+
+  dailyBoxOfficeList = data['boxOfficeResult']['dailyBoxOfficeList']
+  
+  showRange = data['boxOfficeResult']['showRange'][:8]
+  lines = ''
+  for item in dailyBoxOfficeList :
+    #일자,순위,영화제목,매출액,관객수
+    #showRange,rank,movieNm,salesAmt,audiCnt
+    rank = item['rank']
+    movieNm = item['movieNm']
+    salesAmt = item['salesAmt']
+    audiCnt = item['audiCnt']
+    line = f'{showRange},{rank},{movieNm},{salesAmt},{audiCnt}\n'
+    lines = lines + line
+  
+  return lines
+
+
+st.write(box(d))
+
 # Using "with" notation
 with st.sidebar:
     dt_now = datetime.datetime.today()
