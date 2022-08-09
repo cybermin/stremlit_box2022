b6354cc7cf6488214d121751734b8005e1155683
cybermin <sillamin@naver.com>
2022-08-10 08:49:31 +0900

    m

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#from datetime import datetime
+
+from urllib.request import Request, urlopen
+import json  
+ 
 import datetime
 
 
