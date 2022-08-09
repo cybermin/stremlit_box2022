dac45613bd32ef12028584662907b3a3a78c6dc8
cybermin <sillamin@naver.com>
2022-08-10 08:42:52 +0900

    m

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ import datetime
 
 # Using "with" notation
 with st.sidebar:
-    #dt_now = datetime.today()
+    dt_now = datetime.datetime.today()
     
     d = st.date_input(
      "날짜를 선택하세요.",
-     datetime.date(2022, 8, 9))
+     datetime.date(dt_now.year, dt_now.month, dt_now.day))
 
     st.write('선택날짜:', d)
